7f823d8e518d86113d3927e554a29a5d5a16f403
Vegeo Studios <vegeoproductions@gmail.com>
2019-04-26 08:00:32 -0600

    Bug fix

--- index.js
+++ index.js
@@ -232,7 +232,7 @@ client.on('message', message => {
       .addField(message.member.displayName + ' has leveled up!', 'You are now level ' + client.userinfo.get(key, "levelinfo.level"), true)
       .setFooter(((5 * (client.userinfo.get(key, "levelinfo.level") ** 2) + 50 * client.userinfo.get(key, "levelinfo.level") + 100) - client.userinfo.get(key, "levelinfo.xp")) + ' xp to reach level ' + (client.userinfo.get(key, "levelinfo.level") + 1) + '!'));
     Object.keys(ranks).forEach(element => {
-      if (client.userinfo.get(key, "levelinfo.level").toString() === element) member.addRole(message.guild.roles.find(role => role.name === ranks[element]));
+      if (client.userinfo.get(key, "levelinfo.level").toString() === element) message.member.addRole(message.guild.roles.find(role => role.name === ranks[element]));
     });
   }
   //#endregion
