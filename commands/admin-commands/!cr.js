9d5d0b7d13fdf289fda7102eeecb53b0a047ca64
Vegeo Studios <vegeoproductions@gmail.com>
2019-05-12 08:03:16 -0600

    !!cr bug fix

--- commands/admin-commands/!cr.js
+++ commands/admin-commands/!cr.js
@@ -3,7 +3,7 @@ const embedcolor = require('./../../embedcolors.json');
 
 module.exports.run = async (client, message, args) => {
   if (args[0] && args[1]) {
-    client.replies.set(args[0], args[1]);
+    client.replies.set(args[0].toLowerCase(), args[1]);
     message.channel.send(new Discord.RichEmbed().setTitle('CREATE REPLY').setColor(embedcolor["bot"]).setDescription('Reply created successfully'));
   } else {
     message.channel.send(new Discord.RichEmbed().setTitle('CREATE REPLY').setColor(embedcolor["bot"]).setDescription('Make sure you have the correct arguments in your command'));
